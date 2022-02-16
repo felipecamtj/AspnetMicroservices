4c89cd38693f1bc8d3818afbfc6f7fb8750a2fa5
Felipe Campos Pinto <felipecamtj@hotmail.com>
2022-02-16 14:53:30 -0300

    Refatorando projeto mvc

---
+++ src/WebApps/AspnetRunBasics/Models/BasketModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketModelItem> Items { get; set; } = new List<BasketModelItem>();
+
+        public decimal TotalPrice { get; set; }
+    }
+}
